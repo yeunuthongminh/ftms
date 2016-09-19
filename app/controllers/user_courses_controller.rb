226b6de692e5d635dbef44910f6012d552ecd117
tuannd255 <tuannd255@gmail.com>
2016-09-19 07:41:07 +0700

    trainee fix n+1 query

--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -1,10 +1,12 @@
 class UserCoursesController < ApplicationController
   load_and_authorize_resource
+  skip_load_resource only: :show
+  before_action :find_user_course, only: :show
 
   def show
     @course = @user_course.course
     @users = @course.users
-    @user_subjects = @user_course.user_subjects.order_by_course_subject
+    @user_subjects = @user_course.user_subjects.includes(course_subject: :subject).order_by_course_subject
     @trainers = @users.trainers
     @trainees = @users.trainees
     @members = @users.show_members
@@ -14,6 +16,15 @@ class UserCoursesController < ApplicationController
     @user_subject_statuses = UserSubject.statuses
     @user_subject_statuses.each do |key, value|
       instance_variable_set "@number_of_user_subject_#{key}", @user_subjects.send(key).size
+    end
+  end
+
+  private
+  def find_user_course
+    @user_course = UserCourse.includes(:course).find_by_id params[:id]
+    if @user_course.nil?
+      flash[:alert] = flash_message "not_load"
+      redirect_to courses_path
     end
   end
 end
