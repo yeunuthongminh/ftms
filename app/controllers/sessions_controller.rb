264348f001b1477c4ec8442244e3833e62b175f3
tuannd255 <tuannd255@gmail.com>
2016-10-19 10:54:17 +0700

    fix dêt picker and log

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < Devise::SessionsController
 
   def log_sign_out
     log = current_user.track_logs.order_by_time.first
-    log.update_attributes(signout_time: Time.zone.now) if log.signout_time.nil?
+    log.update_attributes(signout_time: Time.zone.now) if log &&
+      log.signout_time.nil?
   end
 end
