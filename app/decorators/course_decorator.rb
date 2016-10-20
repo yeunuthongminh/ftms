a12ef73dafaf0e9e6143a4f5b8857d57389f5153
yeunuthongminh <yeunuthongminh@gmail.com>
2016-10-20 20:48:09 +0700

    fix send mail hot

--- app/decorators/course_decorator.rb
+++ app/decorators/course_decorator.rb
@@ -8,21 +8,21 @@ class CourseDecorator < Draper::Decorator
   end
 
   def show_user_action number
-    show_html = "<div>#{number}: #{I18n.t 'mail.by_day.course',
-      name: self.name}</div><table style='width: 100%;'>
-      <tr>
-        <td style='font-weight: bold; width: 10%;'>
-          #{I18n.t 'mail.by_day.name'}</td>
-        <td style='font-weight: bold; width: 10%;'>
-          #{I18n.t 'mail.by_day.subject'}</td>
-        <td style='font-weight: bold; width: 10%;'>
-          #{I18n.t 'mail.by_day.progress'}</td>
-        <td style='font-weight: bold; width: 20%;'>
-          #{I18n.t 'mail.by_day.task_init'}</td>
-        <td style='font-weight: bold; width: 25%;'>
-          #{I18n.t 'mail.by_day.task_continue'}</td>
-        <td style='font-weight: bold; width: 25%;'>
-          #{I18n.t 'mail.by_day.task_finish'}</td>"
+    show_html = "<div>#{number}: #{I18n.t 'mail.by_day.course', name: self.name}</div>
+      <table style='width: 100%;'>
+        <tr>
+          <td style='font-weight: bold; width: 10%;'>
+            #{I18n.t 'mail.by_day.name'}</td>
+          <td style='font-weight: bold; width: 10%;'>
+            #{I18n.t 'mail.by_day.subject'}</td>
+          <td style='font-weight: bold; width: 10%;'>
+            #{I18n.t 'mail.by_day.progress'}</td>
+          <td style='font-weight: bold; width: 20%;'>
+            #{I18n.t 'mail.by_day.task_init'}</td>
+          <td style='font-weight: bold; width: 25%;'>
+            #{I18n.t 'mail.by_day.task_continue'}</td>
+          <td style='font-weight: bold; width: 25%;'>
+            #{I18n.t 'mail.by_day.task_finish'}</td>"
     user_task_histories = load_user_task_histories self.id
     users = self.users.trainees.to_a
     if user_task_histories.any?
@@ -30,6 +30,7 @@ class CourseDecorator < Draper::Decorator
       users.delete(user)
       subject = user_task_histories.first.user_task.user_subject.subject
       current_status = user_task_histories.first.status
+
       show_html += "</tr><tr>
         <td style='border: 1px solid #cecece;'>
           #{I18n.t 'mail.by_day.user_name', user_name: user.name}
@@ -43,12 +44,21 @@ class CourseDecorator < Draper::Decorator
           progress: user_task_histories.first.user_task.user_subject.percent_progress.to_i}
         </td>
         <td style='border: 1px solid #cecece;'>"
+
+        if current_status == "continue"
+          show_html += "</td><td style='border: 1px solid #cecece;'>"
+        elsif current_status == "finished"
+          show_html += "</td><td style='border: 1px solid #cecece;'>"
+        else
+          show_html += ""
+        end
       user_task_histories.each do |user_task_history|
         unless user_task_history.user_task.user_id == user.id
           user = user_task_history.user_task.user
           users.delete(user)
           subject = user_task_history.user_task.user_subject.subject
-          current_status = "init"
+
+          current_status = user_task_history.status
           show_html += "</td></tr><tr>
             <td style='border: 1px solid #cecece;'>
               #{I18n.t 'mail.by_day.user_name', user_name: user.name}
@@ -58,14 +68,29 @@ class CourseDecorator < Draper::Decorator
             </td>
             <td style='border: 1px solid #cecece;'>
               #{I18n.t 'mail.by_day.progress_done',
-              progress: user_task_history.user_subject.percent_progress.to_i}
+              progress: user_task_history.user_task.user_subject.percent_progress.to_i}
             </td>
             <td style='border: 1px solid #cecece;'>"
+          if current_status == "finished"
+            show_html += "</td><td style='border: 1px solid #cecece;'>"
+          elsif current_status == "continue"
+            show_html += "</td>"
+          elsif current_status == "init"
+            show_html += ""
+          end
         end
         unless user_task_history.user_task.user_subject.subject_id == subject.id
           subject = user_task_history.user_task.user_subject.subject
+          if current_status == "finished"
+            show_html += "</td><td style='border: 1px solid #cecece;'>"
+          elsif current_status == "continue"
+            show_html += "</td><td style='border: 1px solid #cecece;'></td><td style='border: 1px solid #cecece;'>"
+          else
+            binding.pry
+            show_html += ""
+          end
           current_status = user_task_history.status
-          show_html += "</td></tr><tr><td></td>
+          show_html += "</tr><tr><td></td>
             <td style='border: 1px solid #cecece;'>
               #{I18n.t 'mail.by_day.subject_name',
               subject_name: subject.name}
@@ -90,24 +115,29 @@ class CourseDecorator < Draper::Decorator
           show_html += "#{I18n.t 'mail.by_day.continue_task',
             task_name: user_task_history.user_task.task_name}<br>"
         else
-          if current_status == "init"
-            show_html += "</td><td style='border: 1px solid #cecece;'>
-              </td><td style='border: 1px solid #cecece;'>"
-          elsif current_status == "finished" || current_status == "continue"
+          if current_status == "continue"
             show_html += "</td><td style='border: 1px solid #cecece;'>"
+            current_status = "finished"
+          elsif current_status == "finished"
+            show_html += "</td><td style='border: 1px solid #cecece;'>"
+            current_status = "init"
           end
-          current_status = "init"
           show_html += "#{I18n.t 'mail.by_day.finish_task',
             task_name: user_task_history.user_task.task_name}<br>"
         end
       end
     end
 
-    show_html += "</td><td style='border: 1px solid #cecece;'></td>
-      <td style='border: 1px solid #cecece;'>" if current_status == "continue"
+    if current_status == "continue"
+      show_html += "</td><td style='border: 1px solid #cecece;'>"
+    elsif current_status == "finished"
+      show_html += "</td><td style='border: 1px solid #cecece;'></td><td style='border: 1px solid #cecece;'>"
+    else
+      show_html += ""
+    end
 
     unless users.blank?
-      show_html += "</tr>"
+      show_html += "</td></tr>"
       users.each do |user|
         show_html += "<tr>
           <td style='border: 1px solid #cecece;'>
