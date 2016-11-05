965e0a671c874da882a80eb477b6735a7cafc548
TrungNT <hellovietnam93@gmail.com>
2016-11-05 09:55:35 +0700

    Fix exam

--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -2,23 +2,20 @@ class UserSubjectsController < ApplicationController
   load_and_authorize_resource
 
   def update
-    @user_subject.update_status current_user, params["exam"]
+    @user_subject.update_status current_user, "waiting"
     user_course = @user_subject.user_course
-    if params["exam"] == "now"
-      if @user_subject.locked?
-        flash[:alert] = t "exams.lock_for_create"
-        redirect_to user_course_subject_path user_course, @user_subject.subject
-      else
-        exam = ExamServices::NewExamService.new(user_subject: @user_subject).perform
-        if exam
-          redirect_to exam
-        else
-          flash[:alert] = t "exams.not_ready"
-          redirect_to user_course_subject_path user_course, @user_subject.subject
-        end
-      end
-    else
+    if @user_subject.locked?
+      flash[:alert] = t "exams.lock_for_create"
       redirect_to user_course_subject_path user_course, @user_subject.subject
+    else
+      exam = ExamServices::NewExamService.new(user_subject: @user_subject).perform
+      if exam
+        redirect_to exam
+      else
+        flash[:alert] = t "exams.not_ready"
+        redirect_to user_course_subject_path user_course, @user_subject.subject
+      end
     end
   end
 end
+
