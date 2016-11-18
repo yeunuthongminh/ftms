905c37b4de5e5c683ad16f91c2d146cbd179a1c2
TrungNT <hellovietnam93@gmail.com>
2016-11-18 17:00:17 +0700

    Update statistic

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -26,7 +26,7 @@ class Admin::StatisticsController < ApplicationController
     location_ids = params[:location_ids] ? params[:location_ids] : Location.all.map(&:id)
     end_date = params[:end_date] ? params[:end_date].to_date : Date.today
     stage_ids = params[:stage_ids] ? params[:stage_ids] : Stage.all.map(&:id)
-    @statistics = Supports::Statistic.new location_ids: location_ids,
+    @statistics = Supports::StatisticSupport.new location_ids: location_ids,
       start_date: start_date, end_date: end_date, stage_ids: stage_ids
   end
 end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -3,4 +3,9 @@ class Statistic < ApplicationRecord
   belongs_to :stage
   belongs_to :user_type
   belongs_to :programming_language
+
+  delegate :name, to: :user_type, prefix: true, allow_nil: true
+  delegate :name, to: :programming_language, prefix: true, allow_nil: true
+  delegate :name, to: :location, prefix: true, allow_nil: true
+  delegate :name, to: :stage, prefix: true, allow_nil: true
 end
