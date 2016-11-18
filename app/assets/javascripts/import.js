fe2edc9cb80131bdf49c9d9fa49f2e4626150a2c
qtsoul95 <qtsoul95@gmail.com>
2016-11-18 15:47:23 +0700

    Filter statistic trainee by stage

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -1,22 +1,21 @@
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   $('#loading-image').hide();
-  $('input[type=submit]').prop('disabled',true);
-  $('input[type=file]').on('click', function(){
+  $('#import-data input#import-submit').prop('disabled',true);
+  $('#import-data input[type=file]').click(function(){
     $('#loading-image').show();
   });
   $('#file-select').on('change', function() {
     $('#loading-image').hide();
     var file_extension = this.files[0].name.split('.').pop().toLowerCase();
-    var allowed_file;
-    allowed_file = ['csv', 'xls', 'xlsx'];
+    var allowed_file = ['csv', 'xls', 'xlsx'];
     var found_index = $.inArray(file_extension, allowed_file);
     if(found_index < 0) {
       alert(I18n.t('import_data.not_allowed'));
-      $('input[type=submit]').prop('disabled',true);
+      $('#import-data input[type=submit]').prop('disabled',true);
     }
     else{
       alert(I18n.t('import_data.allowed'));
-      $('input[type=submit]').prop('disabled',false);
+      $('#import-data input[type=submit]').prop('disabled',false);
     }
   });
 });
