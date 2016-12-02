2486661f91624642b81bd60cbc911f8dd700d2b4
yeunuthongminh <yeunuthongminh@gmail.com>
2016-12-02 07:44:48 +0700

    â

--- app/models/function.rb
+++ app/models/function.rb
@@ -3,7 +3,4 @@ class Function < ApplicationRecord
   has_many :roles, through: :role_functions
   has_many :user_functions, dependent: :destroy
   has_many :users, through: :user_functions
-
-  scope :has_function, ->(controller, action){where("model_class = ? and action = ?",
-    controller, action)}
 end
--- app/models/user_function.rb
+++ app/models/user_function.rb
@@ -1,4 +1,6 @@
 class UserFunction < ApplicationRecord
   belongs_to :user
   belongs_to :function
+  scope :has_function, ->(controller, action){joins(:function).where("functions.model_class = ?
+    and functions.action = ?", controller, action)}
 end
