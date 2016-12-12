fbb4032804052400c33c1d66c560fa99a5fa8cec
yeunuthongminh <yeunuthongminh@gmail.com>
2016-12-12 10:58:57 +0700

    Fix-orgazination-chart

---
+++ app/controllers/trainer/statistics_controller.rb
@@ -0,0 +1,32 @@
+class Trainer::StatisticsController < ApplicationController
+  include FilterData
+
+  before_action :authorize, except: :show
+  before_action :load_statistic_view
+  before_action :load_filter, only: :show
+
+  def show
+    add_breadcrumb_index "statistics"
+    template = "trainer/statistics/#{params[:type]}"
+    if template_exists? template
+      render template
+    else
+      raise ActionController::RoutingError.new "Not Found"
+    end
+  end
+
+  def create
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+  def load_statistic_view
+    @statistics = "Supports::Statistics::#{params[:type].classify}Support"
+      .constantize.new location_ids: params[:location_ids],
+      check: params[:check_visit], trainee_type_ids: params[:trainee_type_ids],
+      start_date: params[:start_date], end_date: params[:end_date],
+      stage_ids: params[:stage_ids]
+  end
+end
--- db/migrate/20161208020624_rename_prog_language_to_language.rb
+++ db/migrate/20161208020624_rename_prog_language_to_language.rb
@@ -2,8 +2,12 @@ class RenameProgLanguageToLanguage < ActiveRecord::Migration[5.0]
   def change
     rename_table :programming_languages, :languages
     rename_column :profiles, :programming_language_id, :language_id
-    rename_column :categories, :programming_language_id, :language_id
+    remove_foreign_key :categories, column: :programming_language_id
+    rename_column :categories, :programming_language_id, :language_id 
     rename_column :courses, :programming_language_id, :language_id
+    remove_foreign_key :statistics, column: :programming_language_id
     rename_column :statistics, :programming_language_id, :language_id
+    add_foreign_key :categories, :languages, index: true
+    add_foreign_key :statistics, :languages, index: true
   end
 end
