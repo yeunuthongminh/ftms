6027feee466ce40bad09a9b7463ce1294bc8c9d0
Luong Van Sang <sanga2k50@gmail.com>
2016-12-13 16:48:19 +0700

    Number counter in landing page

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -15,15 +15,45 @@ $(document).on('turbolinks:load', function(){
     }
   });
 
-  $('.program-timeline').each(function(index, program) {
-    var total_col = parseInt($(program).data('total-col'));
-    $(program).find('.timeline-block').each(function(index, block) {
-      var col = parseInt($(block).data('col'));
-      $(block).width(col * 100 / total_col + '%');
+  $('.program-timeline').each(function(){
+    var total_col = parseInt($(this).data('total-col'));
+    $(this).find('.timeline-block').each(function(){
+      var col = parseInt($(this).data('col'));
+      $(this).width(col * 100 / total_col + '%');
     });
   });
 
   $('.timeline-body .tooltip').on('hover', function(e){
     $(this).tooltip('bottom');
+  });
+
+  $(window).scroll(function(){
+    $('figure.counter').each(function(index, counter){
+      if($(counter).offset().top < $(window).height() + $(window).scrollTop()){
+        if(parseInt($(counter).data('remaining')) > 0){
+          var number = parseInt($(counter).text());
+          $(counter).prop('Counter', 0).animate({
+            Counter: number
+          },{
+            duration: 1500,
+            easing: 'swing',
+            step: function(now){
+              if(parseInt($(counter).data('remaining')) > 0){
+                var count = Math.ceil(now);
+                $(counter).text(count);
+                $(counter).data('remaining', number - count);
+              }
+            }
+          });
+        }
+      }
+    });
+
+    $('.scroll').each(function(index, section) {
+      if($(section).offset().top < $(window).height() + $(window).scrollTop()){
+        $('a.scroll_to_section').removeClass('active');
+        $('a[data-target="#' + $(section).attr('id') + '"]').addClass('active');
+      }
+    });
   });
 });
