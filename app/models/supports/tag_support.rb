a3832b0a163641d5c31c2159ce509e4c5fd7c76e
Luong Van Sang <sanga2k50@gmail.com>
2017-01-08 15:40:24 +0700

    FAQ refactor and update filter

---
+++ app/models/supports/tag_support.rb
@@ -0,0 +1,37 @@
+class Supports::TagSupport
+  def initialize args = {}
+    @params = args[:params]
+  end
+
+  %w(newest unanswered).each do |post_type|
+    define_method "#{post_type}_posts" do
+      page = @params[:tab] == post_type ? @params[:page] : nil
+      unless instance_variable_get("@#{post_type}_posts")
+        posts = if post_type == "newest"
+          Post.order_desc :created_at
+        else
+          Post.send post_type
+        end.tagged_with(@params[:id]).includes(:taggings, :user)
+        .per_page_kaminari(page).per Settings.faq.posts_per_page
+        instance_variable_set "@#{post_type}_posts", posts
+      end
+      instance_variable_get "@#{post_type}_posts"
+    end
+  end
+
+  %w(day week month).each do |period|
+    define_method "most_viewed_posts_#{period}" do
+      unless instance_variable_get("@most_viewed_posts_#{period}")
+        posts = Post.most_viewed_by(period).tagged_with(@params[:id])
+          .includes(:taggings).take Settings.faq.most_viewed_posts
+        instance_variable_set("@most_viewed_posts_#{period}", posts)
+      end
+      instance_variable_get "@most_viewed_posts_#{period}"
+    end
+  end
+
+  def most_tagged_tags
+    @most_tagged_tags ||= Post.tag_counts_on(:tags).order("COUNT DESC")
+      .limit Settings.faq.most_tagged_tags
+  end
+end
