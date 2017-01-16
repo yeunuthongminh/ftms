bb5b038092b566593784d893e99676fab544f711
Duong Pham <bovame94@gmail.com>
2017-01-16 14:00:45 +0700

    fix: trainer add exam controller, exportort pdf

--- config/routes.rb
+++ config/routes.rb
@@ -114,6 +114,7 @@ Rails.application.routes.draw do
     resources :trainee_evaluations, only: :index
     resources :evaluation_standards
     resources :calendars, only: :index
+    resources :exams, only: [:index, :show]
   end
 
   namespace :assign_user do
